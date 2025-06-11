5871470e1ed8437118c44a57ef4281d1a5abf28e
aliciachickenwings <aachalshresth@gmail.com>
2025-06-11 10:11:49 +0200

    HOME onclick nav to collections

--- src/components/CollectionsContainer.jsx
+++ src/components/CollectionsContainer.jsx
@@ -1,15 +1,19 @@
 import "../css/CollectionsContainer.css";
+import { useNavigate } from "react-router-dom";
 
 export default function CollectionsContainer() {
+  const navigate = useNavigate();
+
   return (
     <div className="collectionsWrapper">
       <div className="collectionsContainer">
-        <div>
-          <img src={"/assets/Collections/img_1.jpg"}></img>
+        <div onClick={() => navigate("/collection")}>
+          <img src={"/assets/Collections/img_1.jpg"} alt="Dirk Lauwaerts" />
           <h3>Dirk Lauwaerts Collectie</h3>
         </div>
-        <div>
-          <img src={"/assets/Collections/img_2.jpg"}></img>
+
+        <div onClick={() => navigate("/collection")}>
+          <img src={"/assets/Collections/img_2.jpg"} alt="Hubert Peeters" />
           <h3>Hubert Peeters Collectie</h3>
         </div>
       </div>
